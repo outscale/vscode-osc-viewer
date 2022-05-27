44eab15c14128125ffd422eac4a52c8a3927281e
Maxime Dufour <maxime.dufour@outscale.com>
2022-05-27 10:24:34 +0000

    Add the name of the Vms

---
+++ src/cloud/vm.ts
@@ -0,0 +1,37 @@
+import * as osc from "outscale-api";
+import { getConfig } from '../cloud/cloud';
+import { Profile } from "../flat/node";
+
+export function getVms(profile: Profile): Promise<Array<osc.Vm> | string> {
+    let config = getConfig(profile);
+    let readParameters : osc.ReadVmsOperationRequest = {
+        readVmsRequest: {}
+    };
+
+    let api = new osc.VmApi(config);
+    return api.readVms(readParameters)
+    .then((res: osc.ReadVmsResponse | string) => {
+        if (typeof res === "string") {
+            return res;
+        }
+        if (res.vms === undefined || res.vms.length === 0) {
+            return "Listing suceeded but it seems you have no vm";
+        }
+        return res.vms;
+    }, (err_: any) => {
+        return "Error, bad credential or region?" + err_;
+    });
+}
+
+export function getVmName(vm: osc.Vm): string {
+    if (typeof vm.tags === "undefined") {
+        return "";
+    }
+
+    for (const tag of vm.tags) {
+        if (tag.key === "name") {
+            return tag.value;
+        }
+    }
+    return "";
+}
--- src/flat/node.folder.vms.ts
+++ src/flat/node.folder.vms.ts
@@ -1,57 +1,32 @@
 import * as vscode from 'vscode';
 import { ExplorerNode, ExplorerFolderNode, Profile } from './node';
 import { FolderNode } from './node.folder';
-import * as osc from "outscale-api";
 import { ResourceNode } from './node.resources';
-import { getConfig } from '../cloud/cloud';
+import { getVmName, getVms } from '../cloud/vm';
 
 
 export class VmsFolderNode extends FolderNode implements ExplorerFolderNode {
     constructor(readonly profile: Profile) {
-		super(profile, "Vms");
+        super(profile, "Vms");
     }
 
-	getChildren(): Thenable<ExplorerNode[]> {
-		return this.getVms().then(vmsResult => {
-			if (typeof vmsResult === "string") {
-				vscode.window.showInformationMessage(vmsResult);
-				return Promise.resolve([]);
-			}
-			let resources = [];
-			for (const vm of vmsResult) {
+    getChildren(): Thenable<ExplorerNode[]> {
+        return getVms(this.profile).then(vmsResult => {
+            if (typeof vmsResult === "string") {
+                vscode.window.showInformationMessage(vmsResult);
+                return Promise.resolve([]);
+            }
+            let resources = [];
+            for (const vm of vmsResult) {
                 if (typeof vm.vmId === 'undefined') {
                     continue;
                 }
-                resources.push(new ResourceNode(this.profile,"", vm.vmId?.toString()));
-			}
-			return Promise.resolve(resources);
-		});
-		
-    }
 
-
-    private async getVms(): Promise<Array<osc.Vm> | string> {
-        let config = getConfig(this.profile);
-        let readParameters : osc.ReadVmsOperationRequest = {
-            readVmsRequest: {}
-        };
-    
-        let api = new osc.VmApi(config);
-        return api.readVms(readParameters)
-        .then((res: osc.ReadVmsResponse | string) => {
-            if (typeof res === "string") {
-                return res;
+                resources.push(new ResourceNode(this.profile, getVmName(vm), vm.vmId?.toString()));
             }
-            if (res.vms === undefined || res.vms.length === 0) {
-                return "Listing suceeded but it seems you have no vm";
-            }
-            return res.vms;
-        }, (err_: any) => {
-            return "Error, bad credential or region?" + err_;
+            return Promise.resolve(resources);
         });
+
     }
-
-
-
 
 }
