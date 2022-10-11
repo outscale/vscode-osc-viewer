014753441efa8f8fb820a93338d3857e34a586e3
Maxime Dufour <maxime.dufour@outscale.com>
2022-10-11 12:15:28 +0000

    Add action on folder to hide it

--- src/flat/folders/node.folder.ts
+++ src/flat/folders/node.folder.ts
@@ -1,14 +1,17 @@
 import * as vscode from 'vscode';
 import { ExplorerNode, ExplorerFolderNode, Profile } from '../node';
 
-
 export abstract class FolderNode implements ExplorerFolderNode {
     constructor(readonly profile: Profile, readonly folderName: string) {
     }
 
+	getContextValue(): string {
+		return "foldernode";
+	}
 
 	getTreeItem(): vscode.TreeItem {
 		const treeItem = new vscode.TreeItem(this.folderName, vscode.TreeItemCollapsibleState.Collapsed);
+		treeItem.contextValue = this.getContextValue();
         return treeItem;
 	}
 
