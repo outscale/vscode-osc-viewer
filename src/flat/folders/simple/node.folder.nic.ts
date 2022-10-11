27f8f4bf7a28ff0b54862859d954e7dfeda0b2db
Maxime Dufour <maxime.dufour@outscale.com>
2022-10-11 09:24:14 +0000

    Add Nic resource

---
+++ src/flat/folders/simple/node.folder.nic.ts
@@ -0,0 +1,33 @@
+import * as vscode from 'vscode';
+import { ExplorerNode, ExplorerFolderNode, Profile } from '../../node';
+import { FolderNode } from '../node.folder';
+import { ResourceNode } from '../../resources/node.resources';
+import { deleteNic, getNics } from '../../../cloud/nics';
+
+export class NicsFolderNode extends FolderNode implements ExplorerFolderNode {
+    constructor(readonly profile: Profile) {
+		super(profile, "Nics");
+    }
+
+	getChildren(): Thenable<ExplorerNode[]> {
+		return getNics(this.profile).then(results => {
+			if (typeof results === "string") {
+				vscode.window.showErrorMessage(results);
+				return Promise.resolve([]);
+			}
+			const resources = [];
+			for (const item of results) {
+                
+                if (typeof item.nicId === 'undefined') {
+                
+                    continue;
+                }
+                
+                resources.push(new ResourceNode(this.profile, "", item.nicId, "Nic", deleteNic));
+                
+			}
+			return Promise.resolve(resources);
+		});
+		
+    }
+}
