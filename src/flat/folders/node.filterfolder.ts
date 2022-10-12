6f8b0cb5c44dad74b43fb30fe87967a9d2bf4d8b
Maxime Dufour <maxime.dufour@outscale.com>
2022-10-12 14:46:28 +0000

    Add FilterFolderNode
    This class will be responsable to add the filtering of resources by
    beeing generic

---
+++ src/flat/folders/node.filterfolder.ts
@@ -0,0 +1,43 @@
+import * as vscode from 'vscode';
+import { FiltersVpnConnection, FiltersNetAccessPoint, FiltersNet, FiltersCa, FiltersSnapshot, FiltersFlexibleGpu, FiltersNic, FiltersDirectLink, FiltersApiAccessRule, FiltersLoadBalancer, FiltersKeypair, FiltersNetPeering, FiltersVolume, FiltersSubnet, FiltersDirectLinkInterface, FiltersClientGateway, FiltersNatService, FiltersSecurityGroup, FiltersRouteTable, FiltersImage, FiltersInternetService, FiltersVm, FiltersVirtualGateway, FiltersPublicIp } from 'outscale-api';
+import { ThemeIcon } from 'vscode';
+import { FILTERS_PARAMETER, getConfigurationParameter, updateConfigurationParameter } from '../../configuration/utils';
+import { Profile } from '../node';
+import { FolderNode } from './node.folder';
+
+export type  FiltersType = FiltersVpnConnection | FiltersNetAccessPoint | FiltersNet | FiltersCa | FiltersSnapshot | FiltersFlexibleGpu | FiltersNic | FiltersDirectLink | FiltersApiAccessRule | FiltersLoadBalancer | FiltersKeypair | FiltersNetPeering | FiltersVolume | FiltersSubnet | FiltersDirectLinkInterface | FiltersClientGateway | FiltersNatService | FiltersSecurityGroup | FiltersRouteTable | FiltersImage | FiltersInternetService | FiltersVm | FiltersVirtualGateway | FiltersPublicIp;
+export abstract class FiltersFolderNode<T> extends FolderNode {
+
+	filters: T | undefined;
+    constructor(readonly profile: Profile, readonly folderName: string) {
+		super(profile, folderName);
+		this.updateFilters();
+    }
+
+    getContextValue(): string {
+        if (typeof this.filters === 'undefined') {
+            return "emptyfilterfoldernode";
+        }
+        return "filterfoldernode";
+	}
+
+    getTreeItem(): vscode.TreeItem {
+		const treeItem = super.getTreeItem();
+        if (typeof this.filters !== 'undefined') {
+            treeItem.iconPath = new ThemeIcon("filter-filled");
+        }
+
+        return treeItem;
+	}
+
+
+	updateFilters(): void {
+        const filters = getConfigurationParameter<any>(FILTERS_PARAMETER);
+        if (this.folderName in filters) {
+            this.filters = this.filtersFromJson(filters[this.folderName]);
+        }
+    }
+
+	abstract filtersFromJson(json: string): T;
+
+}
