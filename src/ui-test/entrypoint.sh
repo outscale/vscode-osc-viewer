e5803ffd4833ca33b1edfd6d96a64691b44fe8c1
Maxime Dufour <maxime.dufour@outscale.com>
2022-10-19 08:10:44 +0000

    Change file permission inside container

--- src/ui-test/entrypoint.sh
+++ src/ui-test/entrypoint.sh
@@ -3,5 +3,8 @@
 # Start Xvfb ffor the display
 Xvfb -ac :99 -screen 0 1920x1080x16 &
 
+# Make all files accessile to the node user
+chown -R node:node /workspace
+
 # Log as node user (1000) and start the test
 su node -c "export DISPLAY=:99; npm run-script ui-test"
