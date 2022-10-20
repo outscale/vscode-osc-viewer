b19877cd9297c45ecc0ae6676042efc21c5530e5
Maxime Dufour <maxime.dufour@outscale.com>
2022-10-20 13:08:34 +0000

    Add randomness on automatic refresh of logs
    - each document has its own refreshment time: interval + random(0,
      interval)
    - minimum refreshment time is 3 seconds
    - default is still 30 seconds

--- src/virtual_filesystem/logs.ts
+++ src/virtual_filesystem/logs.ts
@@ -1,15 +1,19 @@
 import * as vscode from 'vscode';
-import { Observable } from 'rxjs';
+
+import { Observable, Subscription } from 'rxjs';
 import { getLogs } from '../cloud/vms';
 import { getProfile } from '../config_file/utils';
 import { Profile } from '../flat/node';
 
 
+const MIN_REFRESH_INTERVAL = 3;
+const DEFAULT_REFRESH_INTERVAL = 30;
 class LogsDocuments {
     constructor(
         public readonly resourceId: string,
         public readonly profile: Profile,
         public readonly lastData : string,
+        public readonly subscription : Subscription | undefined,
     ) { }
 }
 export class LogsProvider implements vscode.TextDocumentContentProvider {
@@ -17,31 +21,57 @@ export class LogsProvider implements vscode.TextDocumentContentProvider {
     onDidChange = this.onDidChangeEmitter.event;
 
     private _documents = new Map<string, LogsDocuments>();
-    private _subscriptions: vscode.Disposable;
-    private clock$: Observable<void> | undefined;
+
+    private refreshEnabled = false;
+    private refreshInterval = DEFAULT_REFRESH_INTERVAL;
 
     constructor() {
-        this._subscriptions = vscode.workspace.onDidCloseTextDocument(doc => this._documents.delete(doc.uri.toString()));
+        // Let vscode informs us when the document is really closed
+        vscode.workspace.onDidCloseTextDocument(doc => {
+            const logsDocument = this._documents.get(doc.uri.toString());
+            if (typeof logsDocument ===  'undefined') {
+                return;
+            }
+
+            if (typeof logsDocument.subscription !== 'undefined') {
+                logsDocument.subscription.unsubscribe();
+            }
+            this._documents.delete(doc.uri.toString());
+        });
         const conf = vscode.workspace.getConfiguration('osc-viewer');
         const hasParameter = conf.has("refreshConsoleLogs.enabled");
         if (!hasParameter || (hasParameter && conf.get("refreshConsoleLogs.enabled")))  {
+            // 
             const interval = conf.get("refreshConsoleLogs.interval");
-            let intervalNumber = 30;
-            if (typeof interval === 'number') {
+            let intervalNumber = DEFAULT_REFRESH_INTERVAL;
+            if (typeof interval === 'number' && interval >= MIN_REFRESH_INTERVAL) {
                 intervalNumber = interval;
             }
-            this.clock$ = new Observable((subject) => {
-                setInterval(() => {
-                    subject.next();
-                }, 1000 * intervalNumber);
-            });
-            this.clock$.subscribe(() => {
-                for (const uri of this._documents.keys()) {
-                    this.onDidChangeEmitter.fire(vscode.Uri.parse(uri));
-                }
-            });
+            this.refreshEnabled = true;
+            this.refreshInterval= intervalNumber;
         }
 
+    }
+
+    createSubscription(uri: vscode.Uri): Subscription | undefined {
+        if (! this.refreshEnabled) {
+            return undefined;
+        }
+        const refreshInterval = this.refreshInterval;
+        const uriInterval = refreshInterval + (Math.random() * refreshInterval);
+        const observable = new Observable<vscode.Uri>((subject) => {
+            setTimeout(function updateLogs() {
+                if (subject.closed) {
+                    return;
+                }
+                subject.next(uri);
+                setTimeout(updateLogs, 1000 * uriInterval);
+            }, 1000 * uriInterval);
+        });
+        return observable.subscribe((uri: vscode.Uri) => {
+                console.log(uri);
+                this.onDidChangeEmitter.fire(uri);
+        });
     }
 
     async provideTextDocumentContent(uri: vscode.Uri): Promise<string> {
@@ -66,8 +96,10 @@ export class LogsProvider implements vscode.TextDocumentContentProvider {
 
             // Retrieve the resource Type
             resourceId = pathSplit[2];
-            document = new LogsDocuments(resourceId, profile, "");
-            this._documents.set(uri.toString(), document);    
+            const subscription = this.createSubscription(uri);
+            document = new LogsDocuments(resourceId, profile, "", subscription);
+            this._documents.set(uri.toString(), document);
+
         }
         
         return getLogs(profile, resourceId).then((res: string | undefined) => {
