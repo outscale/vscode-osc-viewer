acfd93ab5aede4382b3e6c467f9541b2bdbce776
Maxime Dufour <maxime.dufour@outscale.com>
2022-10-24 13:20:25 +0000

    Test 'Add Profile' button

--- src/ui-test/treeview.ts
+++ src/ui-test/treeview.ts
@@ -36,4 +36,75 @@ describe('ActivityBar', () => {
         expect(title).equals(expectedTitle);
     });
 
+    describe("Add button", async () => {
+        let action: TitleActionButton;
+
+        before(async () => {
+            const expectedCommandName = pjson["contributes"]["commands"].filter((x: any) => x["command"] === "profile.addEntry")[0];
+            action = await titlePart.getAction(expectedCommandName["title"]);
+        });
+
+        it('exists', async () => {
+            expect(action).not.undefined;
+        });
+
+        it('open dialog', async function () {
+            await action.click();
+            const input = new InputBox();
+            // Check the title
+            const title = await input.getTitle();
+            expect(title).equals("Add a profile (1/6)");
+
+            // Check the first action to choose a unique name
+            let message = await input.getMessage();
+            expect(message?.startsWith("Choose a unique name for the profile")).true;
+            await input.setText("vscode");
+            await input.confirm();
+
+            // Get the next one AK
+            message = await input.getMessage();
+            expect(message?.startsWith("Enter your Access Key")).true;
+            await input.setText("AK");
+            await input.confirm();
+
+            // Get the next one SK
+            message = await input.getMessage();
+            expect(message?.startsWith("Enter your Secret Key")).true;
+            await input.setText("SK");
+            await input.confirm();
+
+            // Get the next one region
+            message = await input.getMessage();
+            expect(message?.startsWith("Pick a region")).true;
+            expect(await input.getText()).equals("eu-west-2");
+            await input.confirm();
+
+            // Get the next one host
+            message = await input.getMessage();
+            expect(message?.startsWith("Enter the host")).true;
+            expect(await input.getText()).equals("outscale.com");
+            await input.confirm();
+
+            // Get the next one https
+            const quickPick = await input.getQuickPicks();
+            expect(quickPick.length).equals(2);
+            expect(await quickPick[0].getText()).equals("yes");
+            expect(await quickPick[1].getText()).equals("no");
+            await input.selectQuickPick('yes');
+
+            await delay(1000);
+
+            const notifications = await new Workbench().getNotifications();
+            expect(notifications.length).greaterThan(0);
+            const notification = notifications[0];
+
+            message = await notification.getMessage();
+            expect(message).equals("The account 'vscode' is invalid. Please retry !");
+            expect(await notification.getType()).equals(NotificationType.Error);
+        });
+    });
 });
+
+function delay(milliseconds: number): Promise<number> {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
