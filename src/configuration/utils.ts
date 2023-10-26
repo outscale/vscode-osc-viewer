04533b4e386ec8430bdae509a178bba0d7c2189c
Maxime Dufour <maxime.dufour@outscale.com>
2023-10-26 12:52:48 +0000

    First draft of support osc-cost
    Signed-off-by: Maxime Dufour <maxime.dufour@outscale.com>

--- src/configuration/utils.ts
+++ src/configuration/utils.ts
@@ -5,6 +5,7 @@ const CONFIGURATION_NAME = "osc-viewer";
 
 export const FILTERS_PARAMETER = "filters";
 export const DISABLE_FOLDER_PARAMETER = "disableFolders";
+export const OSC_COST_PARAMETER = "costEstimation";
 
 export function getConfigurationParameter<T>(parameter: string): T | undefined {
     const conf = vscode.workspace.getConfiguration(CONFIGURATION_NAME);
