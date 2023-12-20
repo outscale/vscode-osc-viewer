5850cf9e6a524f4645bc03d326cdd0dedd77e77d
Maxime Dufour <maxime.dufour@outscale.com>
2023-12-20 13:42:00 +0000

    Fix undefined instead of not price
    Signed-off-by: Maxime Dufour <maxime.dufour@outscale.com>

--- src/flat/resources/node.resources.ts
+++ src/flat/resources/node.resources.ts
@@ -43,7 +43,10 @@ export class ResourceNode implements ExplorerResourceNode {
         const treeItem = new vscode.TreeItem(this.resourceId, vscode.TreeItemCollapsibleState.None);
         treeItem.description = this.resourceName;
         if (typeof this.profile.oscCost !== 'undefined') {
-            treeItem.description += " " + this.profile.oscCost.getResourceIdCost(this.resourceType, this.resourceId);
+            const oscCost = this.profile.oscCost.getResourceIdCost(this.resourceType, this.resourceId);
+            if (typeof oscCost !== 'undefined') {
+                treeItem.description += " " + this.profile.oscCost.getResourceIdCost(this.resourceType, this.resourceId);
+            }
         }
         treeItem.iconPath = this.getIconPath();
         treeItem.command = {
